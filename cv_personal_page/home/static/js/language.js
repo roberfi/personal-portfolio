fffc49e9f082efb9023f052864e10e0244516b69
Roberto Fernández Iglesias <roberfi@gmail.com>
2024-11-15 12:20:15 +0100

    Improve language switching

--- cv_personal_page/home/static/js/language.js
+++ cv_personal_page/home/static/js/language.js
@@ -24,35 +24,4 @@ export function initLanguage()
     {
         element.addEventListener("click", hideLanguageMenu);
     });
-
-    // Method to request language change
-    window.changeLanguage = function (url, languageCode)
-    {
-        fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                'X-CSRFToken': csrftoken,
-            },
-            body: new URLSearchParams(
-                {
-                    "language": languageCode,
-                    "next": window.location.pathname.replace(/^\/(en|es)/, ""),
-                }
-            )
-        }).then(response =>
-        {
-            if (response.redirected)
-            {
-                let redirect_url = response.url;
-
-                if (window.location.hash)
-                {
-                    redirect_url += window.location.hash;
-                }
-
-                window.location.href = redirect_url;
-            }
-        });
-    }
 }
