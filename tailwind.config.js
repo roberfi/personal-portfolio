23a0394729f165cc2eab26a238f0df9bd31bd874
Roberto Fernández Iglesias <roberfi@gmail.com>
2025-03-21 10:15:43 +0100

    Bump tailwindcss to v4 and daisyui to v5

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,4 @@ module.exports = {
     "src/frontend/**/*.js",
     "src/*/templates/**/*.html",
   ],
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
-  daisyui: {
-    themes: [
-      {
-        portfolio: {
-          primary: "#fdba74",
-          secondary: "#93c5fd",
-          accent: "#f97316",
-          neutral: "#111827",
-          "base-100": "#4b5563",
-          info: "#08638b",
-          success: "#96c283",
-          warning: "#fef08a",
-          error: "#f87171",
-        },
-      },
-    ],
-  },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,9 @@ module.exports = {
             loader: "postcss-loader",
             options: {
               postcssOptions: {
-                plugins: [[require("tailwindcss"), require("autoprefixer")]],
+                plugins: [
+                  [require("@tailwindcss/postcss"), require("autoprefixer")],
+                ],
               },
             },
           },
