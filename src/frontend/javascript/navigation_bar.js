fdbe61f073121cd9beadcbe48eca3761508814b8
Roberto Fernández Iglesias <roberfi@gmail.com>
2025-03-27 10:55:36 +0100

    Improvements in the Navigation Bar

--- src/frontend/javascript/navigation_bar.js
+++ src/frontend/javascript/navigation_bar.js
@@ -3,12 +3,7 @@ import { smoothScroll } from "./scroll.js";
 export const navigationBar = document.querySelector("#nav-bar");
 
 // Control of navigation menu buttons
-const navigationMenuButton = document.querySelector("#navigation-menu-button");
-const navigationMenu = document.querySelector("#navigation-menu");
-export const navigationMenuLinks =
-  document.querySelectorAll("#navigation-menu a");
-
-const maxMdHiddenClassName = "max-md:hidden";
+export const navigationMenuLinks = document.querySelectorAll("a.section-link");
 
 function handleButtonClick(event) {
   const target = event.target;
@@ -16,16 +11,10 @@ function handleButtonClick(event) {
   event.preventDefault();
   smoothScroll(target.hash);
 
-  if (!navigationMenu.classList.contains(maxMdHiddenClassName)) {
-    navigationMenu.classList.add(maxMdHiddenClassName);
-  }
+  target.blur();
 }
 
 export function initNavigationBar() {
-  navigationMenuButton.addEventListener("click", () => {
-    navigationMenu.classList.toggle(maxMdHiddenClassName);
-  });
-
   // Clicks on navigation menu buttons
   navigationMenuLinks.forEach((element) => {
     element.addEventListener("click", handleButtonClick);
